fbe90f1c68a3528b14399935d324ec116e114a39
Tammy Lam <lamtammy1515@gmail.com>
2020-01-29 19:40:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
-end
+
+def oxford_comma(array)
+    if array.length == 1
+     return array[0]
+    elsif array.length == 2 
+     return array.join(" and ")
+     elsif array.length >= 3 
+     array.insert(array.length-1, "and")
+    return array.join(", ").sub(/.*\K,/, "")
+   end 
+
+end 
+
